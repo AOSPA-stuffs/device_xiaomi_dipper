714e70652320158efa9b8cff1d3dcf83f3a9b877
SH4ND <miqueiasantos20@gmail.com>
2024-09-15 20:06:27 -0300

    dipper: Configure partitions
    Change-Id: I76200a324c23a0122daa58ba7dda80d09fad891e
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>
    Co-authored-by: Yumi Yukimura <me.cafebabe@gmail.com>

--- device.mk
+++ device.mk
@@ -9,6 +9,23 @@
 PRODUCT_VENDOR_PROPERTIES += \
     bluetooth.device.default_name=Xiaomi Mi 8
 
+# Dynamic Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.fastbootd.available=true
+
+# Init
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    fstab.qcom_ramdisk
+
 # Platform
 TARGET_BOARD_PLATFORM := sdm845
 
---
+++ init/Android.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2024 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+# Init scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom_ramdisk
+LOCAL_MODULE_STEM  := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
